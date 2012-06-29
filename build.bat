bc996e962a9d09ae92b185dfdd6c0e1231002c03
Andras Csizmadia <andras@vpmedia.eu>
2012-06-29 14:23:58 +0200

    added assets and build script

---
+++ build.bat
@@ -0,0 +1,22 @@
+::
+:: Copyright 2012, András Csizmadia <andras@vpmedia.eu>.
+::
+
+:: Echo off and begin localisation of Environment Variables
+@ECHO OFF & SETLOCAL
+
+:: Prepare the Command Processor
+VERIFY errors 2>nul
+SETLOCAL ENABLEEXTENSIONS
+IF ERRORLEVEL 1 ECHO Warning: Unable to enable extensions.
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:: Save base directory
+PUSHD %CD%
+::PUSHD %~dp0
+
+:: Set title
+TITLE %~n0
+
+:: STARTUP  
+haxe build.hxml
